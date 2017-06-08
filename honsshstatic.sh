145edb1c2fccdf20e111926bdcc1cc3e8e6eac99
Toni Uhlig <matzeton@googlemail.com>
2017-06-08 02:30:40 +0200

    honssh static startup script

---
+++ honsshstatic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+VNME="honssh-venv"
+VENV="$(dirname $0)/${VNME}"
+
+
+if [ ! -x ${VNME} ]; then
+	virtualenv -p python2.7 "${VENV}"
+fi
+. ${VENV}/bin/activate
+export PYTHONPATH=":${VENV}"
+
+pip install --upgrade twisted geoip watchdog cryptography pyasn1
+twistd -y honssh.tac -p honssh.pid -n
